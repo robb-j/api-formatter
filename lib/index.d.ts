fe8837d3ee8562476b4a6b10e585967cd59c68de
Rob Anderson <robert.anderson2@ncl.ac.uk>
2018-07-20 10:57:23 +0100

    :star: Improved subclass-ability & upgraded dependancies * Added makeEnvelope for subclasses to override the structure * Upgraded dependancies to the latest versions * name/version default to undefined so you can opt-out * Fixed to use typescript types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,34 +1,36 @@
-export declare type Messages = String[] | String;
+export declare type Messages = string[] | string;
 export declare type CatchBlock = (api: Api) => void;
 export declare type ErrorHook = (error: Error, api?: Api) => void;
 export declare class MetaBlock {
-    success: Boolean;
-    messages: String[];
-    status: Number;
-    name?: String;
-    version?: String;
-    constructor(success: Boolean, messages: Messages, status: Number, api: Api);
+    success: boolean;
+    messages: string[];
+    status: number;
+    name?: string;
+    version?: string;
+    constructor(success: boolean, messages: Messages, status: number, api: Api);
 }
 export interface IApiOptions {
-    name?: String;
-    version?: String;
-    httpFail?: Boolean;
+    name?: string;
+    version?: string;
+    httpFail?: boolean;
 }
 /** A class for creating a json envelope for sending api responses */
 export declare class Api {
     /** Creates an express middleware that adds req.api & res.api instances */
     static middleware(options: IApiOptions): (req: any, res: any, next: () => void) => void;
-    private req;
-    private res;
-    name?: String;
-    version?: String;
-    httpFail: Boolean;
+    protected req: any;
+    protected res: any;
+    name?: string;
+    version?: string;
+    httpFail: boolean;
     /** Creates a new Api instance from an Express responses */
     constructor(req: any, res: any, options?: IApiOptions);
     /** Sends an api failure */
-    sendFail(messages: Messages, status?: Number): void;
+    sendFail(messages: Messages, status?: number): void;
     /** Sends a data response */
-    sendData(data: any, status?: Number): void;
+    sendData(data: any, status?: number): void;
     /** Catches errors and sends the message as a sendFail */
     catch(block: CatchBlock, hook?: ErrorHook): Promise<void>;
+    protected pickStatusCode(status?: number): number;
+    protected makeEnvelope(success: boolean, messages: Messages, status: number, data: any): any;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-export type Messages = String[] | String
+export type Messages = string[] | string
 export type CatchBlock = (api: Api) => void
 export type ErrorHook = (error: Error, api?: Api) => void
 
 export class MetaBlock {
   
-  public success: Boolean
-  public messages: String[]
-  public status: Number
-  public name?: String
-  public version?: String
+  public success: boolean
+  public messages: string[]
+  public status: number
+  public name?: string
+  public version?: string
   
-  constructor(success: Boolean, messages: Messages, status: Number, api: Api) {
+  constructor(success: boolean, messages: Messages, status: number, api: Api) {
     this.success = success
-    this.messages = (typeof messages === 'string') ? [<String>messages] : <String[]>messages
+    this.messages = (typeof messages === 'string') ? [messages] : messages
     this.status = status
     this.name = api.name
     this.version = api.version
@@ -20,16 +20,16 @@ export class MetaBlock {
 }
 
 export interface IApiOptions {
-  name?: String
-  version?: String
-  httpFail?: Boolean
+  name?: string
+  version?: string
+  httpFail?: boolean
 }
 
 /** A class for creating a json envelope for sending api responses */
 export class Api {
   
   /** Creates an express middleware that adds req.api & res.api instances */
-  // static middleware(name: String = null, version: String = null) {
+  // static middleware(name: string = null, version: string = null) {
   static middleware(options: IApiOptions) {
     return (req: any, res: any, next: () => void) => {
       let api = new this(req, res, options)
@@ -39,38 +39,35 @@ export class Api {
     }
   }
   
-  private req: any
-  private res: any
-  public name?: String
-  public version?: String
-  public httpFail: Boolean
+  protected req: any
+  protected res: any
+  
+  public name?: string
+  public version?: string
+  public httpFail: boolean
   
   /** Creates a new Api instance from an Express responses */
   constructor(req: any, res: any, options: IApiOptions = {}) {
     this.req = req
     this.res = res
-    this.name = options.name || process.env.npm_package_name || null
-    this.version = options.version || process.env.npm_package_version || null
+    this.name = options.name || process.env.npm_package_name || undefined
+    this.version = options.version || process.env.npm_package_version || undefined
     this.httpFail = options.httpFail === undefined ? true : options.httpFail
   }
   
   /** Sends an api failure */
-  public sendFail(messages: Messages, status?: Number) {
-    if (status === undefined) {
-      status = this.httpFail ? 400 : 200
-    }
-    this.res.status(status).send({
-      meta: new MetaBlock(false, messages, status, this),
-      data: null
-    })
+  public sendFail(messages: Messages, status?: number) {
+    status = this.pickStatusCode(status)
+    this.res.status(status).send(
+      this.makeEnvelope(false, messages, status, null)
+    )
   }
   
   /** Sends a data response */
-  public sendData(data: any, status: Number = 200) {
-    this.res.status(status).send({
-      meta: new MetaBlock(true, [], status, this),
-      data: data
-    })
+  public sendData(data: any, status: number = 200) {
+    this.res.status(status).send(
+      this.makeEnvelope(true, [], status, data)
+    )
   }
   
   /** Catches errors and sends the message as a sendFail */
@@ -81,6 +78,19 @@ export class Api {
     catch (error) {
       if (hook) hook(error, this)
       this.sendFail(error.message)
+    }
+  }
+  
+  protected pickStatusCode(status?: number): number {
+    return status === undefined
+      ? this.httpFail ? 400 : 200
+      : status
+  }
+  
+  protected makeEnvelope(success: boolean, messages: Messages, status: number, data: any): any {
+    return {
+      meta: new MetaBlock(success, messages, status, this),
+      data
     }
   }
 }
